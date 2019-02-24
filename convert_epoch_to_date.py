53ce120769fc6bfdd83d1db9b19330a078c2882d
Vikas Kasera <19317423+nexional@users.noreply.github.com>
2019-02-24 07:54:17 +0000

    added various user options - custom date format, disable inline replacement, show message box

--- convert_epoch_to_date.py
+++ convert_epoch_to_date.py
@@ -3,30 +3,45 @@ import sublime_plugin
 import re
 from datetime import datetime
 
+def getSettings(string):
+    return sublime.load_settings('ConvertEpochToDate.sublime-settings').get(string)
 
 class ConvertEpochToDateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        view = self.view
+        view = self.view; msg = ''
         for region in view.sel():
             if region.empty():
                 region = view.word(region)
 
             epoch_text = view.substr(region)
             if epoch_text and re.match(r'^(\d{10,13})$', epoch_text):
-                result = datetime.fromtimestamp(int(str(epoch_text)[0:10])).strftime("%a %d %b %Y %I:%M:%S %p")
+
+                output_date_format = getSettings('output_date_format')
+                try:
+                    result = datetime.fromtimestamp(int(epoch_text[0:10])).strftime(output_date_format)
+                except ValueError:
+                    msg = ' (Warning: Invalid format string \'' + output_date_format + '\' defined, using default)'
+                    output_date_format = '%a %d %b %Y %I:%M:%S %p'
+                    result = datetime.fromtimestamp(int(epoch_text[0:10])).strftime(output_date_format)
 
                 if result:
-                    milsec = str(epoch_text)[10:13]
-                    if milsec:
-                        milsec = format(int(float('0.' + milsec) * 1000), '03d')
-                        result = re.sub(r' [AP]M$', '.' + str(milsec) + '\g<0>', result)
+                    show_ms = getSettings('show_milliseconds')
+                    if show_ms:
+                        milsec = epoch_text[10:13]
+                        if not milsec:
+                            milsec = '0'
+                        milsec = milsec.ljust(3, '0')
+                        result = result + ' + ' + milsec + 'ms'
 
                     if view.is_read_only():
-                        sublime.status_message('ConvertEpochToDate: ' + result + ' (Warning: View readonly. Can\'t make inline replacement)')
+                        msg = ' (Warning: View readonly. Can\'t make inline replacement)'
                     else:
                         view.replace(edit, region, result)
-                        sublime.status_message('ConvertEpochToDate: ' + result)
+
+                    msg = 'ConvertEpochToDate: ' + result + msg
                 else:
-                    sublime.status_message('ConvertEpochToDate: Conversion error')
+                    msg = 'ConvertEpochToDate: Conversion error'
             else:
-                sublime.status_message('ConvertEpochToDate: Invalid epoch timestamp: \'' + epoch_text + '\'. Must be 10-13 digits')
+                msg = 'ConvertEpochToDate: Invalid epoch timestamp: \'' + epoch_text + '\'. Must be 10-13 digits'
+
+            sublime.status_message(msg)
