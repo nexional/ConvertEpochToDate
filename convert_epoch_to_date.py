c61a0e3400dab7afdda48e0af5683134e1d84d4e
nexional <nexional@gmail.com>
2020-11-10 17:40:34 +0000

    formatting

--- convert_epoch_to_date.py
+++ convert_epoch_to_date.py
@@ -3,12 +3,15 @@ import sublime_plugin
 import re
 from datetime import datetime
 
-def getSettings(string):
+
+def get_settings(string):
     return sublime.load_settings('ConvertEpochToDate.sublime-settings').get(string)
+
 
 class ConvertEpochToDateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        view = self.view; msg = ''
+        view = self.view
+        msg = ''
         for region in view.sel():
             if region.empty():
                 region = view.word(region)
@@ -16,7 +19,7 @@ class ConvertEpochToDateCommand(sublime_plugin.TextCommand):
             epoch_text = view.substr(region)
             if epoch_text and re.match(r'^(\d{10,13})$', epoch_text):
 
-                output_date_format = getSettings('output_date_format')
+                output_date_format = get_settings('output_date_format')
                 try:
                     result = datetime.fromtimestamp(int(epoch_text[0:10])).strftime(output_date_format)
                 except ValueError:
@@ -25,22 +28,22 @@ class ConvertEpochToDateCommand(sublime_plugin.TextCommand):
                     result = datetime.fromtimestamp(int(epoch_text[0:10])).strftime(output_date_format)
 
                 if result:
-                    if getSettings('show_milliseconds'):
+                    if get_settings('show_milliseconds'):
                         milsec = epoch_text[10:13]
                         if not milsec:
                             milsec = '0'
                         result = result + ' + ' + milsec.ljust(3, '0') + 'ms'
 
                     if view.is_read_only():
-                        if not getSettings('show_message_box') and getSettings('show_message_box_for_readonly'):
+                        if not get_settings('show_message_box') and get_settings('show_message_box_for_readonly'):
                             sublime.message_dialog(result)
-                        msg = ' (Warning: View readonly' + ( '' if not getSettings('in_place_replacement') else '. Can not make in-place replacement' ) + ')'
-                    elif not getSettings('in_place_replacement'):
+                        msg = ' (Warning: View readonly' + ('' if not get_settings('in_place_replacement') else '. Can not make in-place replacement') + ')'
+                    elif not get_settings('in_place_replacement'):
                         msg = ' (Warning: In-place replacement disabled)'
                     else:
                         view.replace(edit, region, result)
 
-                    if getSettings('show_message_box'):
+                    if get_settings('show_message_box'):
                         sublime.message_dialog(result)
 
                     msg = 'ConvertEpochToDate: ' + result + msg
